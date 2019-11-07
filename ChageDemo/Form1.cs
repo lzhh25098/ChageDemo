e018eac94c77e0191124b2b71166b4745ba3a28c
刘志华 <2509687281@qq.com>
2019-11-07 19:36:43 +0800

    完成登录验证

--- ChageDemo/Form1.cs
+++ ChageDemo/Form1.cs
@@ -16,6 +16,10 @@ namespace ChageDemo
         {
             InitializeComponent();
         }
+        //用户名
+        public static string uName = "chenhui";
+        //密码
+        public static string passWorld = "123456";
 
         private void frmLogin_Load(object sender, EventArgs e)
         {
@@ -25,7 +29,22 @@ namespace ChageDemo
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.Opacity = 0.8;
+            string userName = textBox1.Text.Trim();
+            string pwd = textBox2.Text.Trim();
+            if (userName.Equals(uName))
+            {
+                if (passWorld.Equals(pwd))
+                {
+                    MessageBox.Show("登录成功!欢迎你!" + uName);
+                    this.Hide();
+                }
+                else {
+                    MessageBox.Show("密码不正确!");
+                }
+            }
+            else {
+                MessageBox.Show("用户名不正确!");
+            }
         }
     }
 }
